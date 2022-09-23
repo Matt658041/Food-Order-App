678db6c4049679c0fe94a4d4b5f2a1127fd48c12
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-09-23 09:43:07 -0500

    added useReducer logic to update state of cart

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import CartIcon from '../Cart/CartIcons'
-import classes from './HeaderCartButton.module.css'
-
+import React from "react";
+import CartIcon from "../Cart/CartIcons";
+import classes from "./HeaderCartButton.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-  return (
-      <button className={classes.button} onClick={props.onClick}>
-          <span className={classes.icon}>
-              <CartIcon/>
-          </span> 
-          <span>Your Cart</span>
-          <span className={classes.badge}>
-              3
-          </span>
-    </button>
-  )
-}
+  const cartCtx = useContext(CartContext);
 
-export default HeaderCartButton
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems }</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
